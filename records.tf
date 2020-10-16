8badd56980a3a45a1edb3cad0f79324a9f0e9977
ZeePal <pallsjh@gmail.com>
2020-10-16 19:58:30 +1100

    updated mx to Google Domain forwarders

--- records.tf
+++ records.tf
@@ -30,14 +30,19 @@ resource google_dns_record_set a_records {
   rrdatas = each.value
 }
 
+# https://support.google.com/domains/answer/9428703
 resource google_dns_record_set mx_records {
   name         = google_dns_managed_zone.zone.dns_name
   managed_zone = google_dns_managed_zone.zone.name
 
   type = "MX"
-  ttl  = 3 * 24 * 60 * 60 # 3 Days to Seconds
+  ttl  = 60 * 60 # 1 Hour
 
   rrdatas = [
-    "5 mail.${var.domain}."
+    "5 gmr-smtp-in.l.google.com.",
+    "10 alt1.gmr-smtp-in.l.google.com.",
+    "20 alt2.gmr-smtp-in.l.google.com.",
+    "30 alt3.gmr-smtp-in.l.google.com.",
+    "40 alt4.gmr-smtp-in.l.google.com."
   ]
 }
