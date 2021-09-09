af001d7a11e8e58e02a396388bd2dbd61b6dc484
ZeePal <pallsjh@gmail.com>
2021-09-09 15:32:15 +1000

    updated to dyndns

--- records.tf
+++ records.tf
@@ -1,37 +1,5 @@
-locals {
-  primary_ips = ["59.167.196.35"]
-
-  dns_a_records = {
-    ""  = local.primary_ips
-    www = local.primary_ips
-
-    webmail = local.primary_ips
-    mail    = local.primary_ips
-    smtp    = local.primary_ips
-    pop3    = local.primary_ips
-    imap    = local.primary_ips
-
-    k9   = local.primary_ips
-    doc  = local.primary_ips
-    sql  = local.primary_ips
-    ldap = local.primary_ips
-  }
-}
-
-
-resource google_dns_record_set a_records {
-  for_each = local.dns_a_records
-
-  managed_zone = google_dns_managed_zone.zone.name
-  type         = "A"
-  ttl          = 3 * 24 * 60 * 60 # 3 Days to Seconds
-
-  name    = "${each.key != "" ? "${each.key}." : ""}${google_dns_managed_zone.zone.dns_name}"
-  rrdatas = each.value
-}
-
 # https://support.google.com/domains/answer/9428703
-resource google_dns_record_set mx_records {
+resource "google_dns_record_set" "mx_records" {
   name         = google_dns_managed_zone.zone.dns_name
   managed_zone = google_dns_managed_zone.zone.name
 
@@ -45,4 +13,13 @@ resource google_dns_record_set mx_records {
     "30 alt3.gmr-smtp-in.l.google.com.",
     "40 alt4.gmr-smtp-in.l.google.com."
   ]
+}
+
+resource "google_dns_record_set" "a_records" {
+  managed_zone = google_dns_managed_zone.zone.name
+  type         = "CNAME"
+  ttl          = 3 * 24 * 60 * 60 # 3 Days to Seconds
+
+  name    = "games.${google_dns_managed_zone.zone.dns_name}"
+  rrdatas = "zeepal.freeddns.org."
 }
