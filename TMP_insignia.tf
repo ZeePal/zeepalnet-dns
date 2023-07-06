b48cfde300e10e854c22c2551ef4e89c22d6ce2a
Paul Harrington <pallsjh@gmail.com>
2023-07-06 17:35:29 +1000

    TEMP: added experimental/sandbox public zone for insignia personal poc (dnssec)

--- TMP_insignia.tf
+++ TMP_insignia.tf
@@ -11,3 +11,14 @@ resource "google_dns_record_set" "TMP_insignia" {
     "ns-cloud-c4.googledomains.com.",
   ]
 }
+
+resource "google_dns_record_set" "TMP_insignia_dnssec" {
+  managed_zone = google_dns_managed_zone.zone.name
+  type         = "DS"
+  ttl          = 60
+
+  name = "gcp.insigniafinancial.sandbox.${google_dns_managed_zone.zone.dns_name}"
+  rrdatas = [
+    "20417 8 2 C384D9CA0E96BBF5163DA512D34ABEBDF454CF4AED7E38A6FF00603B62B6EF50",
+  ]
+}
