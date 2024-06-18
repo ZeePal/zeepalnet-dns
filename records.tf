a949217d702664f491752148b48d1624001edbc5
Paul Harrington <pallsjh@gmail.com>
2024-06-18 15:07:44 +1000

    step 1 migration to squarespace email forwarding

--- records.tf
+++ records.tf
@@ -11,7 +11,9 @@ resource "google_dns_record_set" "mx_records" {
     "10 alt1.gmr-smtp-in.l.google.com.",
     "20 alt2.gmr-smtp-in.l.google.com.",
     "30 alt3.gmr-smtp-in.l.google.com.",
-    "40 alt4.gmr-smtp-in.l.google.com."
+    "40 alt4.gmr-smtp-in.l.google.com.",
+    "100 mxa.mailgun.org.", # TODO: after ~24h remove above records and reduce priority to 10
+    "100 mxb.mailgun.org.", # TODO: after ~24h remove above records and reduce priority to 10
   ]
 }
 
